5aadbad4353436e278e5ed0fc14d3f9de54e3e0e
frysee <frysee@googlemail.com>
2025-04-07 17:29:06 +0200

    Properly deploy trimui SDK, no matter which architecture

--- support/setup-toolchain.sh
+++ support/setup-toolchain.sh
@@ -1,10 +1,5 @@
 #! /bin/sh
 
-TOOLCHAIN_ARCH=`uname -m`
-if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
-	exit
-fi
-
 SYSROOT_TAR="SDK_usr_tg5040_a133p"
 TOOLCHAIN_NAME="aarch64-linux-gnu"
 TOOLCHAIN_TAR="gcc-arm-8.3-2019.02-x86_64-aarch64-linux-gnu"
@@ -14,13 +9,22 @@ SYSROOT_URL="https://github.com/trimui/toolchain_sdk_smartpro/releases/download/
 
 cd ~
 
-wget $TOOLCHAIN_URL
-wget $SYSROOT_URL
+TOOLCHAIN_ARCH=`uname -m`
+if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
+	wget $SYSROOT_URL
 
-tar xf $TOOLCHAIN_TAR.tar.xz -C /opt
-mv /opt/$TOOLCHAIN_TAR /opt/$TOOLCHAIN_NAME
-rm $TOOLCHAIN_TAR.tar.xz
+	tar xf $SYSROOT_TAR.tgz
+	rsync -a --ignore-existing ./usr/ /usr/
+	rm -rf ./usr $SYSROOT_TAR.tgz
+else 
+	wget $TOOLCHAIN_URL
+	wget $SYSROOT_URL
 
-tar xf $SYSROOT_TAR.tgz
-rsync -a --ignore-existing ./usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
-rm -rf ./usr $SYSROOT_TAR.tgz
+	tar xf $TOOLCHAIN_TAR.tar.xz -C /opt
+	mv /opt/$TOOLCHAIN_TAR /opt/$TOOLCHAIN_NAME
+	rm $TOOLCHAIN_TAR.tar.xz
+
+	tar xf $SYSROOT_TAR.tgz
+	rsync -a --ignore-existing ./usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
+	rm -rf ./usr $SYSROOT_TAR.tgz
+fi
